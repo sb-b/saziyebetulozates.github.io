482b71cd0df3b39cee884e9e90d228bfc00798f8
Betül Özateş <sbetulbilgin@gmail.com>
2024-09-08 16:15:57 +0300

    Create script.js

---
+++ _layouts/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const profilePic = document.getElementById('profile-pic');
+    const originalSrc = profilePic.src; // Automatically uses the config.yml value for current-pic
+    const hoverSrc = "{{ site.hover_logo }}"; // Fetch hover-pic from config.yml
+
+    // When mouse enters the image area
+    profilePic.addEventListener('mouseover', function() {
+        profilePic.src = hoverSrc;
+    });
+
+    // When mouse leaves the image area
+    profilePic.addEventListener('mouseout', function() {
+        profilePic.src = originalSrc;
+    });
+});
